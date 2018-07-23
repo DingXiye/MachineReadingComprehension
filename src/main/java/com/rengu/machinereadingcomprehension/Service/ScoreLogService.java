422323ad7577747781ae792b7e7722205eebfa95
韩常铭 <han.changming@qq.com>
2018-07-23 13:44:21 +0800

    结果提交时添加请求

--- src/main/java/com/rengu/machinereadingcomprehension/Service/ScoreLogService.java
+++ src/main/java/com/rengu/machinereadingcomprehension/Service/ScoreLogService.java
@@ -1,14 +1,14 @@
 package com.rengu.machinereadingcomprehension.Service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rengu.machinereadingcomprehension.Entity.ScoreLogEntity;
 import com.rengu.machinereadingcomprehension.Entity.UserEntity;
 import com.rengu.machinereadingcomprehension.Repository.ScoreLogRepository;
-import okhttp3.*;
+import com.rengu.machinereadingcomprehension.Utils.Metric;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +17,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @program: MachineReadingComprehension
@@ -38,56 +40,18 @@ public class ScoreLogService {
     }
 
     // 保存成绩历史
-    public Map<String, Double> saveScoreLog(MultipartFile ref, MultipartFile pred, UserEntity userEntity) throws IOException {
-        // 接收文件
-        File refFile = new File(FileUtils.getTempDirectoryPath() + UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(ref.getOriginalFilename()));
-        FileUtils.copyInputStreamToFile(ref.getInputStream(), refFile);
-        File predFile = new File(FileUtils.getTempDirectoryPath() + UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(pred.getOriginalFilename()));
-        FileUtils.copyInputStreamToFile(pred.getInputStream(), predFile);
-        // 获取成绩
-        OkHttpClient okHttpClient = new OkHttpClient();
-
-        RequestBody refBody = RequestBody.create(MediaType.parse("application/json"), refFile);
-        RequestBody predBody = RequestBody.create(MediaType.parse("application/json"), predFile);
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("ref_result", ref.getOriginalFilename(), refBody)
-                .addFormDataPart("pred_result", pred.getOriginalFilename(), predBody)
-                .build();
-
-        Request request = new Request.Builder()
-                .url("http:///47.96.153.138:8083/test")
-                .addHeader("content-type", "multipart/form-data")
-                .addHeader("Cache-Control", "no-cache")
-                .post(requestBody)
-                .build();
-        Response response = okHttpClient.newCall(request).execute();
-        // 解析结果
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(response.body().string());
-        ScoreLogEntity scoreLogEntity = new ScoreLogEntity();
-        scoreLogEntity.setUserEntity(userEntity);
-        // 产生随机数据
-        scoreLogEntity.setBLEU_4_Score(jsonNode.path("BLEU-4").asDouble());
-        scoreLogEntity.setROUGE_Score(jsonNode.path("ROUGE-L").asDouble());
-        Map<String, Double> resultMap = new HashMap<>();
-        resultMap.put("BLEU_4", scoreLogEntity.getBLEU_4_Score());
-        resultMap.put("ROUGE", scoreLogEntity.getROUGE_Score());
-        scoreLogRepository.save(scoreLogEntity);
-        return resultMap;
-    }
-
-    // 保存成绩历史
     public ScoreLogEntity saveScoreLog(UserEntity userEntity, MultipartFile multipartFile, int type) throws IOException {
         // 接收文件
         File resultFile = new File(FileUtils.getUserDirectoryPath() + "/User_Result/" + userEntity.getTeamName() + "_" + simpleDateFormat.format(new Date()) + "." + FilenameUtils.getExtension(multipartFile.getOriginalFilename()));
         FileUtils.copyInputStreamToFile(multipartFile.getInputStream(), resultFile);
-        Random random = new Random();
+        Resource resource = new ClassPathResource("question.json");
+        File answerFile = resource.getFile();
+        ConcurrentHashMap<String, Double> resultMap = Metric.getScore(answerFile, resultFile);
         ScoreLogEntity scoreLogEntity = new ScoreLogEntity();
         scoreLogEntity.setUserEntity(userEntity);
         scoreLogEntity.setType(type);
-        scoreLogEntity.setBLEU_4_Score(random.nextDouble() * 100);
-        scoreLogEntity.setROUGE_Score(random.nextDouble() * 100);
+        scoreLogEntity.setBLEU_4_Score(resultMap.get("bleu_score") * 100);
+        scoreLogEntity.setROUGE_Score(resultMap.get("rouge_score") * 100);
         return scoreLogRepository.save(scoreLogEntity);
     }
 
